b3ad842b2838dc9b8d80b04d9a3bfabaa7925b4e
zainabraza06 <zainabraza1960@gmail.com>
2026-05-19 22:46:19 +0500

    udpated url in mobile

--- nexus_mobile/lib/config.dart
+++ nexus_mobile/lib/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // Emulator: 10.0.2.2 maps to host machine localhost
   // Physical device: change to your machine's LAN IP e.g. http://192.168.1.x:8000
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'https://insightflow-backend-481589186819.us-central1.run.app';
 }
